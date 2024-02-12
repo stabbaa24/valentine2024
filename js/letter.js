b08d3cf0d8d96b669a7fa7e4af65f63f9cd9005c
stabbaa <siham.tabbaa@etu.unice.fr>
2024-02-13 00:58:11 +0100

    Letter KO

--- js/letter.js
+++ js/letter.js
@@ -1,18 +1,5 @@
-function openEnvelope() {
-    const flap = document.getElementById('flap');
-    const letter = document.getElementById('letter');
-
-    flap.style.transform = 'rotateX(180deg)';
-    setTimeout(() => {
-        letter.classList.add('open');
-    }, 500); // Temps pour commencer l'animation après que le flap est ouvert.
+// script.js
+function ouvrirEnveloppe() {
+    let enveloppe = document.getElementById("enveloppe");
+    enveloppe.classList.toggle("enveloppe-ouverte");
 }
-
-// Ajoutez ce script pour gérer les interactions avec le cœur
-document.addEventListener('DOMContentLoaded', () => {
-    const heart = document.querySelector('.heart');
-
-    heart.addEventListener('click', () => {
-        openEnvelope();
-    });
-});
